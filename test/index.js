dbb3bdbd9475977288042398084e422c37b98bfd
hejunze <hejunze@guanmai.cn>
2020-12-04 17:48:12 +0800

    update: add a example about babel action

--- test/index.js
+++ test/index.js
@@ -1,13 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
-import { transformFromAst, transformFileSync } from '@babel/core';
+import { transformFromAst, transformFileSync, transform } from '@babel/core';
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
+import * as t from '@babel/types'
 import * as babylon from 'babylon';
 import trim from './helpers/trim.js'
-// import plugin from '../src';
 
-
-describe('文件夹-测试用例', () => {
+describe('测试用例目录', () => {
   const fixturesDir = path.join(path.resolve(), 'test', 'fixtures');
     before(() => {
       console.log(`---开始读取测试用例文件夹 - ${fixturesDir}`)
@@ -33,4 +34,41 @@ describe('文件夹-测试用例', () => {
   });
 });
 
-// describe('')
+describe('独立测试用例', () => {
+  describe('babel 转换操作', () => {
+    context('添加兄弟节点', () => {
+      it('insertBefore', () => {
+        const code = 'var a;'
+        const rightResult = `var b;\nvar a;`
+        const ast = parser.parse(code)
+        traverse(ast, {
+          enter(path) {
+            if (t.isVariableDeclaration(path.node)) {
+              const node = t.variableDeclaration('var', [t.variableDeclarator(t.identifier('b'))])
+              path.insertBefore(node)
+              path.stop()
+            }
+          }
+        });
+        const result = transformFromAst(ast).code
+        assert.equal(result, rightResult)
+      })
+      it('insertAfter', () => {
+        const code = 'var a;'
+        const rightResult = `var a;\nvar b;`
+        const ast = parser.parse(code)
+        traverse(ast, {
+          enter(path) {
+            if (t.isVariableDeclaration(path.node)) {
+              const node = t.variableDeclaration('var', [t.variableDeclarator(t.identifier('b'))])
+              path.insertAfter(node)
+              path.stop() // 注意要加 stop 方法，因为在你添加一样的节点后，节点遍历会进入新的节点，如果没加stop，会导致死循环。
+            }
+          }
+        });
+        const result = transformFromAst(ast).code
+        assert.equal(result, rightResult)
+      })
+    })
+  })
+})
