d6cd647816397d501cbb6a02a718537062bc3fef
hejunze <hejunze@guanmai.cn>
2020-12-04 11:23:57 +0800

    update: add some order about mocha

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # babel_demo
+
+> 关于babel相关的学习及练习
+
+### 运行说明
+
+测试
+
+```
+npm run test
+
+npm run test:watch 监听
+
+npm run report 报告
+```
+
+编译
+```
+npm run build
+```
--- test/index.js
+++ test/index.js
@@ -3,37 +3,34 @@ import fs from 'fs';
 import assert from 'assert';
 import { transformFromAst, transformFileSync } from '@babel/core';
 import * as babylon from 'babylon';
+import trim from './helpers/trim.js'
 // import plugin from '../src';
 
 
-function trim(str) {
-  return str.replace(/^\s+|\s+$/, '');
-}
-
-describe('Allow to reduce boilerplate of writing async actions', () => {
+describe('文件夹-测试用例', () => {
   const fixturesDir = path.join(path.resolve(), 'test', 'fixtures');
+    before(() => {
+      console.log(`---开始读取测试用例文件夹 - ${fixturesDir}`)
+    })
+    after(() => {
+      console.log(`---结束读取测试用例文件夹 - ${fixturesDir}`)
+    })
   fs.readdirSync(fixturesDir).map((caseName) => {
-    it(`should ${caseName.split('-').join(' ')}`, () => {
-      const fixtureDir = path.join(fixturesDir, caseName);
-      const actualPath = path.join(fixtureDir, 'actual.js');
-      const actual = transformFileSync(actualPath).code;
-      /*const actual = transformFromAst(
-        babylon.parse(
-          fs.readFileSync(actualPath).toString(),
-          {
-            sourceType: "module",
-            plugins: "*"
-          }
-        ),
-        "",
-        JSON.parse(fs.readFileSync(path.join(fixtureDir, ".babelrc"), "utf8"))
-      ).code;*/
+    const fixtureDir = path.join(fixturesDir, caseName);
+    const actualPath = path.join(fixtureDir, 'actual.js');
 
+    beforeEach(function(){
+      console.log(`-----测试用例 - ${fixtureDir}`);
+    })
+
+    it(`should ${caseName.split('-').join(' ')}`, () => {
+      const actual = transformFileSync(actualPath).code;
       const expected = fs.readFileSync(
           path.join(fixtureDir, 'expected.js')
       ).toString();
-
       assert.equal(trim(actual), trim(expected));
-    });
+    })
   });
 });
+
+// describe('')
